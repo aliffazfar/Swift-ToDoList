d235b72003fa008f2b67b662ddacf5d9f068f001
Aliff Azfar  @aleph-labs <aliff.azfar@aleph-labs.com>
2023-07-09 22:27:01 +0800

    [Fixed] Added error message for authentication

--- ToDoList/ViewModels/LoginViewViewModel.swift
+++ ToDoList/ViewModels/LoginViewViewModel.swift
@@ -19,7 +19,21 @@ class LoginViewViewModel: ObservableObject {
             return
         }
         //Try log in
-        Auth.auth().signIn(withEmail: email, password: password)
+        Auth.auth().signIn(withEmail: email, password: password) { (auth, error) in
+            if let x = error {
+                  let err = x as NSError
+                  switch err.code {
+                  case AuthErrorCode.wrongPassword.rawValue:
+                      self.errorMessage = "Wrong password, you big dummy"
+                  case AuthErrorCode.invalidEmail.rawValue:
+                      self.errorMessage = "Invalid email - duh"
+                  case AuthErrorCode.accountExistsWithDifferentCredential.rawValue:
+                      self.errorMessage = "The account already exists"
+                  default:
+                      self.errorMessage = "\(err.localizedDescription)"
+                  }
+              }
+        }
     }
     
     private func validate() -> Bool{
